a24e6ac78d5ff923ee254ac0143f07e0fbd65e0a
kirby-b <kirby.brodie66@gmail.com>
2023-08-18 23:09:29 -0400

    Setting up
    - Added the base html
    - Added base js(class names, variables)
    - added css "classes" (put names with brackets for elements, ids, and classes)
    -Connected it all

---
+++ RPS.js
@@ -0,0 +1,19 @@
+const playerText = document.querySelector("#playerText");
+const compText = document.querySelector("#compText");
+const resText = document.querySelector("#resText");
+const choiceBtns = document.querySelectorAll(".choiceBtn");
+let player;
+let comp;
+
+choiceBtns.forEach(button => button.addEventListener("click", () => {
+
+    
+}));
+
+function compTurn(){
+
+    
+}
+function checkWinner(){
+ 
+}
